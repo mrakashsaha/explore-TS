036a60b13f7d023b46274068950a12f9a7c6330e
Akash Kumar Saha <mr.akashsaha@gmail.com>
2025-05-27 14:50:59 +0600

    Learn about interface in objects

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,35 @@
-import { SportsMan } from "./classes/Player.js";
+/*function drawRactrangle (options : {width: number, length:number}) {
+    let width = options.width;
+    let length = options.length;
+}
 
-const sportMan1 = new SportsMan ("Akash", 465, "Bangladesh");
+
+drawRactrangle({width: 46, length: 546})
+
+*/
+
+// Above task we can do with interface.
+
+interface RectangleOptions {
+    width: number,
+    length: number,
+    height: number,
+}
 
 
-console.log (sportMan1.name)
+function drawRactrangle (options : RectangleOptions) {
+    let width = options.width;
+    let length = options.length;
+}
+
+
+
+
+drawRactrangle ({
+    width: 40, 
+    height: 20,
+    length: 40,
+})
+
+
+
---
+++ src/script10.ts
@@ -0,0 +1,6 @@
+import { SportsMan } from "./classes/Player.js";
+
+const sportMan1 = new SportsMan ("Akash", 465, "Bangladesh");
+
+
+console.log (sportMan1.name)
